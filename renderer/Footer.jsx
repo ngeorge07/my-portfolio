a14c2777367b148adfd846f2bb51a62a5ec7e6c1
George <geor2335@stud.kea.dk>
2022-03-30 12:16:59 +0200

    added contact

--- renderer/Footer.jsx
+++ renderer/Footer.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Logo from "./Logo";
+import { MdEmail } from "react-icons/md";
+import { BsLinkedin } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
 
 function Footer() {
   return (
@@ -8,7 +11,27 @@ function Footer() {
       <div className="footer-contact">
         <h3>Contact</h3>
         <ul>
-          <li>E-mail: georgenico9@gmail.com</li>
+          <li>
+            <a href="mailto:georgenico9@gmail.com" target="_blank">
+              <MdEmail />
+              georgenico9@gmail.com
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/georgenicolae07/"
+              target="_blank"
+            >
+              <BsLinkedin />
+              @georgenicolae07
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/ngeorge07" target="_blank">
+              <BsGithub />
+              @ngeorge07
+            </a>
+          </li>
         </ul>
       </div>
     </footer>
