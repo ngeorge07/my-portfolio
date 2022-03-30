fe9dc1daba6acb31985ff84d47f288a7f7b45dd9
George <geor2335@stud.kea.dk>
2022-03-30 11:12:49 +0200

    implemented individual project component with props obj

--- components/IndividualProject.jsx
+++ components/IndividualProject.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { Link } from "./Link";
+import { FaExternalLinkAlt } from "react-icons/fa";
 
-function IndividualProject() {
-  return <div>IndividualProject</div>;
+function IndividualProject({ children, liveLink, repoLink, photo, title }) {
+  return (
+    <>
+      <section className="project-intro">
+        <div className="project-links">
+          <Link className="project-away-link" href={liveLink} target="_blank">
+            Live demo
+            <FaExternalLinkAlt />
+          </Link>
+          <Link className="project-away-link" href={repoLink} target="_blank">
+            GitHub repo
+            <FaExternalLinkAlt />
+          </Link>
+        </div>
+
+        <img src={photo} alt="" />
+      </section>
+
+      <h2 className="project-title">{title}</h2>
+
+      <section className="double-side-text">{children}</section>
+    </>
+  );
 }
 
 export default IndividualProject;
--- pages/projects/robochat/index.page.jsx
+++ pages/projects/robochat/index.page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import TextSection from "../../../components/TextSection";
-import { Link } from "../../../components/Link";
-import { FaExternalLinkAlt } from "react-icons/fa";
+import IndividualProject from "../../../components/IndividualProject";
+
 import pictureChat from "/robo-chat.png";
 
 export { Page };
@@ -10,55 +10,37 @@ export const documentProps = {
 };
 
 function Page() {
+  const props = {
+    title: "Robo Chat",
+    liveLink: "https://robo-chat.netlify.app/",
+    repoLink: "https://github.com/ngeorge07/chatty-app",
+    photo: `${pictureChat}`,
+  };
+
   return (
-    <>
-      <section className="project-intro">
-        <div className="project-links">
-          <Link
-            className="project-away-link"
-            href="https://robo-chat.netlify.app/"
-            target="_blank"
-          >
-            Live demo
-            <FaExternalLinkAlt />
-          </Link>
-          <Link
-            className="project-away-link"
-            href="https://github.com/ngeorge07/chatty-app"
-            target="_blank"
-          >
-            GitHub repo
-            <FaExternalLinkAlt />
-          </Link>
-        </div>
+    <IndividualProject {...props}>
+      <TextSection title="Tech stack">
+        <p>
+          For the past year I've build projects using HTML, CSS and JavaScript.
+          After I felt more than comfortable working with vanilla CSS and Js I
+          went further and thought myself how to work more efficiently using
+          SASS and React. I would like to work using React or any JS framework.
+        </p>
+      </TextSection>
 
-        <img src={pictureChat} alt="" />
-      </section>
+      <span className="vertical-line"></span>
 
-      <h2 className="project-title">Robo Chat</h2>
-      <section className="double-side-text">
-        <TextSection title="Tech stack">
-          <p>
-            For the past year I've build projects using HTML, CSS and
-            JavaScript. After I felt more than comfortable working with vanilla
-            CSS and Js I went further and thought myself how to work more
-            efficiently using SASS and React. I would like to work using React
-            or any JS framework.
-          </p>
-        </TextSection>
-        <span className="vertical-line"></span>
-        <TextSection title="About">
-          <p>
-            In time, I made my own strategy of debugging to maximize the
-            learning process. I start by googling the error to see what's
-            causing it. If it's a syntax error I search it on MDN and read the
-            documentation. If it's something else I would keep looking for
-            answers online for one hour. If I cant't find a solution I would ask
-            a teacher. With this approach, even if I don't solve an issue I
-            still learn new valuable information.
-          </p>
-        </TextSection>
-      </section>
-    </>
+      <TextSection title="About">
+        <p>
+          In time, I made my own strategy of debugging to maximize the learning
+          process. I start by googling the error to see what's causing it. If
+          it's a syntax error I search it on MDN and read the documentation. If
+          it's something else I would keep looking for answers online for one
+          hour. If I cant't find a solution I would ask a teacher. With this
+          approach, even if I don't solve an issue I still learn new valuable
+          information.
+        </p>
+      </TextSection>
+    </IndividualProject>
   );
 }
