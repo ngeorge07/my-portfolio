10ca47f679688ff9f19f79db8f36fafaae5cd6eb
George <geor2335@stud.kea.dk>
2022-03-30 10:47:44 +0200

    added title to project page

--- pages/index.page.jsx
+++ pages/index.page.jsx
@@ -34,20 +34,16 @@ function Page() {
             For the past year I've build projects using HTML, CSS and
             JavaScript. After I felt more than comfortable working with vanilla
             CSS and Js I went further and thought myself how to work more
-            efficiently using <span className="accent-color"> SASS</span> and
-            <span className="accent-color"> React</span>. I would like to work
-            using React or any JS
-            <span className="accent-color"> framework</span>.
+            efficiently using SASS and React. I would like to work using React
+            or any JS framework.
           </p>
         </TextSection>
         <span className="vertical-line"></span>
         <TextSection title="My process">
           <p>
-            In time, I made my own strategy of debugging to
-            <span className="accent-color"> maximize</span> the learning
-            process. I start by googling the error to see what's causing it. If
-            it's a syntax error I search it on
-            <span className="accent-color"> MDN</span> and read the
+            In time, I made my own strategy of debugging to maximize the
+            learning process. I start by googling the error to see what's
+            causing it. If it's a syntax error I search it on MDN and read the
             documentation. If it's something else I would keep looking for
             answers online for one hour. If I cant't find a solution I would ask
             a teacher. With this approach, even if I don't solve an issue I
