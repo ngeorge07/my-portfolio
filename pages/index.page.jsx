8adfb0810dfdd059ad5c60a75c13e72fa3bbf74a
George <georgenico9@gmail.com>
2023-02-20 02:34:32 +0100

    update portfolio

--- pages/index.page.jsx
+++ pages/index.page.jsx
@@ -11,74 +11,94 @@ import data from "./projects/projects.json";
 export { Page };
 
 function Page() {
-  return (
-    <>
-      <section className="intro-section">
-        <div>
-          <h2>
-            Hi, I'm George and I am a multimedia design student specialized in{" "}
-            <span className="accent-color">front-end development.</span>
-          </h2>
+	return (
+		<>
+			<section className="intro-section">
+				<div>
+					<h2>
+						Hi, I'm George and I am a{" "}
+						<span className="accent-color">front-end developer</span> with a
+						multimedia design background.
+					</h2>
 
-          <Link className="main-button" href="/projects">
-            See all my projects
-          </Link>
-        </div>
+					<Link className="main-button" href="/projects">
+						See all my projects
+					</Link>
+				</div>
 
-        <img src={profilePic} alt="" />
-      </section>
+				<img src={profilePic} alt="" />
+			</section>
 
-      <section className="double-side-text">
-        <TextSection title="My skills">
-          For the past year I have built several projects using HTML, CSS, as
-          well as JavaScript. After I started feeling comfortable working with
-          vanilla CSS and Js, I focused on further improving my skills by
-          teaching myself to work more efficiently using SASS and React. I am
-          looking forward to working using React or any JS framework.
-        </TextSection>
-        <span className="vertical-line"></span>
-        <TextSection title="My process">
-          In time, I made my own strategy of debugging to maximize the learning
-          process. I start by googling the error to see what's causing it. If
-          it's a syntax error I search it on MDN and read the documentation. If
-          it's something else I would keep looking for answers online for one
-          hour. If I cant't find a solution I would ask a teacher. With this
-          approach, even if I don't solve an issue I still learn new valuable
-          information.
-        </TextSection>
-      </section>
+			<section className="double-side-text">
+				<TextSection title="My skills">
+					I am an accomplished developer with extensive experience in JavaScript
+					frameworks like React and <span className="accent-color">Next</span>,
+					and styling libraries such as{" "}
+					<span className="accent-color">Tailwind</span> and ChakraUI. My coding
+					practices prioritize clean, safe, and scalable code, and I work with
+					BaaS products like Firebase and{" "}
+					<span className="accent-color">Supabase</span> for seamless backend
+					integration. I am dedicated to continuous learning to deliver
+					high-quality solutions that meet client business objectives. Check my{" "}
+					<a
+						href="https://github.com/ngeorge07"
+						target="_blank"
+						className="inline-link">
+						GitHub
+					</a>{" "}
+					profile pinned projects for a display of my skills.
+				</TextSection>
+				<span className="vertical-line"></span>
+				<TextSection title="My experience">
+					I am an skilled front-end developer with a strong track record of
+					delivering solutions. I recently graduated with top honors from KEA -
+					Copenhagen School of Design and Development with an AP degree in
+					Multimedia Design. I possess a keen eye for good UI/UX layouts and a
+					good understanding of web design principles. Currently, I work as a
+					full-time developer at{" "}
+					<a
+						href="https://www.tinyrhino.dk/"
+						target="_blank"
+						className="inline-link">
+						TinyRhino
+					</a>{" "}
+					, where I develop both web and native applications using React and
+					React Native. My strong analytical and problem-solving skills enable
+					me to deliver high-quality solutions within tight deadlines, while
+					maintaining a strong focus on client satisfaction.
+				</TextSection>
+			</section>
 
-      <ProjectsContainer title="Project Showcase">
-        <div className="double-side">
-          {data.slice(0, 4).map((projectObj) => (
-            <ProjectCard
-              url={projectObj.url}
-              imgPath={projectObj.imgPath}
-              title={projectObj.title}
-              key={projectObj.id}
-            >
-              <p>{projectObj.text}</p>
-            </ProjectCard>
-          ))}
-        </div>
+			<ProjectsContainer title="Project Showcase">
+				<div className="double-side">
+					{data.slice(0, 4).map((projectObj) => (
+						<ProjectCard
+							url={projectObj.url}
+							imgPath={projectObj.imgPath}
+							title={projectObj.title}
+							key={projectObj.id}>
+							<p>{projectObj.text}</p>
+						</ProjectCard>
+					))}
+				</div>
 
-        <Link className="main-button" href="/projects">
-          See all my projects
-        </Link>
-      </ProjectsContainer>
+				<Link className="main-button" href="/projects">
+					See all my projects
+				</Link>
+			</ProjectsContainer>
 
-      <section>
-        <h3>Tech I work with</h3>
-        <div className="skills-section">
-          <Skill imgUrl={"/skills/html.svg"} caption="HTML" />
-          <Skill imgUrl={"/skills/css.svg"} caption="CSS" />
-          <Skill imgUrl={"/skills/sass.svg"} caption="SASS" />
-          <Skill imgUrl={"/skills/js.svg"} caption="JavaScript" />
-          <Skill imgUrl={"/skills/react.svg"} caption="React" />
-          <Skill imgUrl={"/skills/tailwind.svg"} caption="Tailwind" />
-          <Skill imgUrl={"/skills/git.svg"} caption="Git" />
-        </div>
-      </section>
-    </>
-  );
+			<section>
+				<h3>Tech I work with</h3>
+				<div className="skills-section">
+					<Skill imgUrl={"/skills/html.svg"} caption="HTML" />
+					<Skill imgUrl={"/skills/css.svg"} caption="CSS" />
+					<Skill imgUrl={"/skills/sass.svg"} caption="SASS" />
+					<Skill imgUrl={"/skills/js.svg"} caption="JavaScript" />
+					<Skill imgUrl={"/skills/react.svg"} caption="React" />
+					<Skill imgUrl={"/skills/tailwind.svg"} caption="Tailwind" />
+					<Skill imgUrl={"/skills/git.svg"} caption="Git" />
+				</div>
+			</section>
+		</>
+	);
 }
